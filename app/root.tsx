2d1a89ef9aaddf4176044f8dec54a19658766931
Thomas Cullen <thomascullen92@gmail.com>
2022-10-14 12:36:40 +0100

    Update meta tag

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import styles from "./tailwind.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Thomas Cullen",
   viewport: "width=device-width,initial-scale=1",
 });
 
